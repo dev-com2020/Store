989b8f3008c701dcf79e4cd9ef864dd057655fb3
DEV-COM <kwp@tlen.pl>
2022-02-03 16:02:13 +0100

    pierwszy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-2 text-white">
+  Tu powstaje aplikacja Store.
+  </div>`
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Store';
+ 
 }
